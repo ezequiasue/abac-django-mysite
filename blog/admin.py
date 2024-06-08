ea98229c8045a973cf77c033c12fd15eb289a578
Ezequias Silva Santos <phd.esantos@gmail.com>
2024-06-07 22:50:47 -0400

    feat: change class admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_on')
     search_fields = ('title', 'author')
-    list_filter = ('created_on', 'author')
+    list_filter = ('created_on', 'author', 'status')
     autocomplete_fields = ('author',)
     
 admin.site.register(Post,PostAdmin)
